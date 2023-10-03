d687b78f6d62a343d2f0214f29ddce485406fefc
RaymondLam18 <1024322@hr.nl>
2023-10-03 10:18:33 +0200

    aanmaak registreer en verifieer, verwijder profile files

--- resources/views/profile.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.web')
-
-@section('PageTitle', 'Profile')
-@section('content')
-    <body>
-    <h1>Profile</h1>
-    <a href="{{ url('/home') }}">Go to Home</a>
-    <a href="{{ url('/about') }}">Go to About</a>
-@endsection
