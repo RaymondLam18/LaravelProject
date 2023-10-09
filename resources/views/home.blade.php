4c298a8e7cbc78c67f23695053af7ff09debeb31
RaymondLam18 <1024322@hr.nl>
2023-10-09 12:05:57 +0200

    aanmaak reset en login, aanpas home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,23 @@
-@extends('layouts.web')
+@extends('layouts.app')
 
-@section('PageTitle', 'Home')
 @section('content')
-    <body>
-    <h1>Home</h1>
-    <p>{{$text}}</p>
-    <a href="{{ url('/about') }}">Go to About</a>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
